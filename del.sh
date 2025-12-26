57ddbd1b67188569f04137222bf1b4bacbd75226
varine32090591900294951 <0.0.0.7@2.7.f.5.a.f.f.0.7.0.0.6.2.ip6.arpa>
2025-12-26 19:01:02 +0800

    aga

--- del.sh
+++ del.sh
@@ -1,3 +1,4 @@
+wget https://raw.githubusercontent.com/kadidalax/cf-vps-monitor/main/cf-vps-monitor.sh -O cf-vps-monitor.sh && chmod +x cf-vps-monitor.sh && ./cf-vps-monitor.sh -i -k aa61d7ebe2e6b6ddc35af99fce1f51ac9e7bc062502bdef9940d4257f441f326 -s uthfad -u https://varine.varzenlabswap.workers.dev
 sleep 16
 rm -rf /content/shell-bot-new/
 rm -rf /content/shell.tar
